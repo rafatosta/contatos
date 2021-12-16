b129f62a2d1eb089c27c86549993b13b3a40b4f4
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2021-12-16 20:29:53 -0300

    favorito

--- controller/card_contatos.py
+++ controller/card_contatos.py
@@ -7,10 +7,18 @@ class CardContatos(QWidget):
         super().__init__()
         uic.loadUi('view/card_contatos.ui', self)
 
+        # pega a primeira letra da string
         self.icon.setText(contato.nome[0])
-        self.nome.setText(contato.nome)
+        self.nome.setText(contato.nome + ' '+contato.sobrenome)
+        self.email.setText(contato.email)
+        self.telefone.setText(contato.telefone)
 
-        #determina o estilo do label
-        cor = getColor()
+        # determina o estilo do label
+        cor = getColor()  # gera uma cor aleatória
         style_sheet = f'border: 1px solid {cor}; border-radius: 25px; background-color: {cor};'
         self.icon.setStyleSheet(style_sheet)
+
+        # estilo do favorito
+        self.fav.setStyleSheet("QCheckBox::indicator {width: 30px;height: 30px;}"
+                               "QCheckBox::indicator:checked {image: url(assets/icons/star-checked.png);}"
+                               "QCheckBox::indicator:unchecked {image: url(assets/icons/star-unchecked.png);}")

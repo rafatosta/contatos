8dac6b87c58626ab29aa72325dd09b82c3fe1d9b
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2021-12-18 17:33:25 -0300

    Título e ícone da janela

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from controller.main_window import MainWindow
 os.environ['XDG_SESSION_TYPE'] = 'Wayland'
 
 app = QApplication(sys.argv)
+app.setWindowIcon(QIcon('assets/icons/contatos.png'))
 win = MainWindow()
 win.show()
 sys.exit(app.exec())
